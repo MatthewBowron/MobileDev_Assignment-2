96ba3fe3a2780925eb85c4fef2fe7159f266b4cc
Marcos Vinicius Berger Gilles <viniciuscla2015@gmail.com>
2025-06-16 21:43:59 -0600

    improve login logic and comments

--- components/signIn/sign-in.tsx
+++ components/signIn/sign-in.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { FunctionComponent, useEffect, useState } from "react";
 import { StyleSheet, TouchableOpacity, Text, View, TextInput } from "react-native";
-import  handleSignIn  from "./sign-inFactory";
+import  validateSignIn  from "./sign-inFactory";
 
 interface Props { 
     navigate: ()=>void; 
@@ -10,6 +10,7 @@ interface Props {
 const SignIn: FunctionComponent<Props> = ({navigate}) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  
   var errorMessages = ["", "", ""]
   
   useEffect(() => {
@@ -19,20 +20,21 @@ const SignIn: FunctionComponent<Props> = ({navigate}) => {
     errorMessages[1] = "";
   }, [password]) // Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character.
  
+
   const cleanUp = () => {
     setUsername("");
     setPassword("");
   }
 
-  const handleSubmit = () => {
-    if (handleSignIn(username, password)) {
+
+  const handleSubmit = () => { // handles the form submission, validates the input, and navigates to the next screen if successful
+    if (validateSignIn(username, password)) {
       navigate();
     }
     else {
-      cleanUp();
+      cleanUp(); // cleans up the input fields
       alert("Invalid username or password. Please try again.");
     }
-
   }
 
     
@@ -40,25 +42,37 @@ const SignIn: FunctionComponent<Props> = ({navigate}) => {
       <View style={styles.container}>
         <Text style={styles.header}>Sign In to App</Text>
 
+
         <TextInput 
           style={styles.input}
           placeholder="Username"
           value={username}
           onChangeText={setUsername}
         />
+
+
         <Text style={styles.errorMssg}>{errorMessages[0]}</Text>
+        
+        
         <TextInput 
           style={styles.input}
           placeholder="Password"
           value={password}
           onChangeText={setPassword}
         />
+        
+        
         <Text style={styles.errorMssg}>{errorMessages[1]}</Text>
 
+        
         <TouchableOpacity onPress={handleSubmit}>
             <Text style={styles.button}>Submit</Text>
         </TouchableOpacity>
+        
+        
         <Text style={styles.errorMssg}>{errorMessages[2]}</Text>
+      
+      
       </View>
     )
 }
--- components/signIn/sign-inFactory.tsx
+++ components/signIn/sign-inFactory.tsx
@@ -1,13 +1,16 @@
+// This file contains the logic for validating user sign-in credentials
 
 import { navigate } from "expo-router/build/global-state/routing"
 import users from "../../credentials.json"
 
-export default function handleSignIn(username:String, password:String){
-    // This function would typically handle the login logic
+
+export default function validateSignIn(username:String, password:String){
+    // This function validate if username and password are in the credentials.json file
+    // and return true if they are, otherwise return false
 
     for (let user of users.users) {
         if (user.username === username && user.password === password) {
-            // Simulate successful login
+            // if username and password return true
             return true
         }
     }
